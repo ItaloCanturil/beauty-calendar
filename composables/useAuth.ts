a5bae0a88a5e27a4c0b363271e8a1e7ff1fa4dd3
Italo Canturil <italo.cantur@gmail.com>
2024-10-15 16:11:07 -0300

    refactor: change dotenv url

--- composables/useAuth.ts
+++ composables/useAuth.ts
@@ -8,8 +8,8 @@ export const useAuth = () => {
     const redirectLocal = localStorage.getItem('redirectTo');
 
     let url =
-      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+      process?.env?.NUXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+      `${process?.env?.NUXT_PUBLIC_VERCEL_URL}${{redirectLocal}}` ?? // Automatically set by Vercel.
       `http://localhost:3000${redirectLocal}` ??
       'http://localhost:3000/'
     // Make sure to include `https://` when not localhost.
