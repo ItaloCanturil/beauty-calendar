499170077b6b2ba6b6afc72fb1f8e49f42a5444d
Italo Canturil <italo.cantur@gmail.com>
2025-03-23 08:24:17 -0300

    feat(auth):  add route guard

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,6 +1,21 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-  if (process.client) {
-    localStorage.setItem('redirectTo', to.fullPath);
-  }
+export default defineNuxtRouteMiddleware(async (to, from) => {
+	const { isLogged } = useAuth();
+	const { profile } = useProfileStore();
 
-})
+	if (!isLogged) {
+		return navigateTo("/");
+	}
+
+	const adminRoutes = [
+		"dashboard",
+		"profile-id",
+		"profile-id-hours",
+		"profile-id-settings",
+	];
+
+	if (adminRoutes.includes(to.name as string)) {
+		if (profile?.role !== "admin") {
+			return navigateTo("/");
+		}
+	}
+});
