e2059d0f76c3ffc028eb2d10d4c9f1db07aefc37
Italo Canturil <italo.cantur@gmail.com>
2025-03-28 11:49:10 -0300

    refactor: reform the hero component

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,35 +1,24 @@
-<script setup lang="ts"> 
-const emit = defineEmits<{
-  (e: 'login'): void;
-  (e: 'new'): void;
-}>();
+<script setup lang="ts">
 
-
-const { isLogged } = useAuth();
+  const emit = defineEmits<{
+    (e: 'login'): void;
+  }>();
 </script>
 
 <template>
-  <div
-    class="w-full h-[calc(100vh-300px)] text-center px-5 flex flex-col justify-center items-center gap-2"
-  >
-    <h1>
-      Comece agora a gerenciar e facilitar os agendamentos com seus clientes
+  <div class="w-full h-full text-center px-5 flex flex-col justify-center items-center gap-2">
+    <figure class="absolute inset-0 -z-10">
+      <NuxtImg src="/public/img/hero.png" alt="Mulher" class="w-full h-auto object-cover" />
+    </figure>
+
+    <div class="flex-1"></div>
+
+    <h1 class="text-white text-4xl">
+      Simplifique o agendamento do seu negócio
     </h1>
 
-    
-    <Button
-      v-if="isLogged"
-      label="Adicionar novo horário"
-      icon="pi pi-plus"
-      @click="() => emit('new')"
-    />
 
-    <Button
-      v-else
-      class="mt-5"
-      label="Começar agora"
-      icon="pi pi-plus"
-      @click="() => emit('login')"
-    />
+    <Button class="mt-5 mb-20" label="Começar agora" icon="pi pi-plus" @click="() => emit('login')" />
   </div>
+
 </template>
