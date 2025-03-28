051fb87b722ccab05729f4b9ff5a6ef6b3a62fd3
Italo Canturil <italo.cantur@gmail.com>
2025-03-28 11:31:08 -0300

    build: add `motion` and `nuxt/images`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,38 +1,40 @@
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  vite: {
-    vue: {
-      customElement: true
-    },
-    vueJsx: {
-      mergeProps: true
-    }
-  },
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@pinia/nuxt',
-    "@nuxtjs/supabase",
-    '@vueuse/nuxt',
-    '@primevue/nuxt-module'
-  ],
-  primevue: {
-    importTheme: { from: './assets/css/preset.ts'},
-  },
-  pinia: {
-    storesDirs: ['./stores/**', './custom-folder/stores/**'],
-  },
-  supabase: {
-    url: process.env.SUPABASE_URL,
-    key: process.env.SUPABASE_KEY,
-    serviceKey: process.env.SUPABASE_SERVICE_KEY,
-    redirect: false,
-    types: './schema.ts'
-  },
-  runtimeConfig: {
-    public: {
-      urlSite: process.env.NUXT_PUBLIC_VERCEL_URL
-    }
-  }
-})
+	compatibilityDate: "2024-04-03",
+	devtools: { enabled: false },
+	css: ["~/assets/css/main.css"],
+	vite: {
+		vue: {
+			customElement: true,
+		},
+		vueJsx: {
+			mergeProps: true,
+		},
+	},
+	modules: [
+		"@nuxtjs/tailwindcss",
+		"@pinia/nuxt",
+		"@nuxtjs/supabase",
+		"@vueuse/nuxt",
+		"@primevue/nuxt-module",
+		"@nuxt/image",
+		"motion-v/nuxt",
+	],
+	primevue: {
+		importTheme: { from: "./assets/css/preset.ts" },
+	},
+	pinia: {
+		storesDirs: ["./stores/**", "./custom-folder/stores/**"],
+	},
+	supabase: {
+		url: process.env.SUPABASE_URL,
+		key: process.env.SUPABASE_KEY,
+		serviceKey: process.env.SUPABASE_SERVICE_KEY,
+		redirect: false,
+		types: "./schema.ts",
+	},
+	runtimeConfig: {
+		public: {
+			urlSite: process.env.NUXT_PUBLIC_VERCEL_URL,
+		},
+	},
+});
