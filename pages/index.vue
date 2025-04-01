1981f7e548a8d0ccd6d194f4030d5de1dfeffbae
Italo Canturil <italo.cantur@gmail.com>
2025-04-01 14:33:12 -0300

    feat: use `signin` middleware

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+  definePageMeta({
+    middleware: 'signin'
+  })
+
   const router = useRouter();
-  const useProfile = useProfileStore();
-  const profileId = computed(() => useProfile.profile!.id);
   const visible = ref(false);
 
   const { loginWithProvider } = useAuth();
@@ -24,10 +26,5 @@
   <div class="w-full h-full">
     <Hero @login="loginWithProvider" @new="() => router.push(`/dashboard`)" />
 
-    <!-- <Dialog v-model:visible="visible" modal header="Não encontrado">
-      <span class="text-surface-500 dark:text-surface-400 block mb-8">Solicite o link ao profissional novamente</span>
-    </Dialog> -->
-
-    <!-- <LoginModal :active="showLogin" @close="(boo) => showLogin = boo"/> -->
   </div>
 </template>
