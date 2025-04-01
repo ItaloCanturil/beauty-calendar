d53d3613301ad11082bedc6ee5a8dd9c62fc5987
Italo Canturil <italo.cantur@gmail.com>
2025-04-01 11:28:02 -0300

    refactor(error): add robust to error

--- composables/useAuth.ts
+++ composables/useAuth.ts
@@ -1,73 +1,67 @@
 import { get } from "@vueuse/core";
 
 export const useAuth = () => {
-  const user = useSupabaseUser();
-  const client = useSupabaseClient();
+	const user = useSupabaseUser();
+	const client = useSupabaseClient();
 
-  const isLogged = computed(() => get(user) !== null);
+	const isLogged = computed(() => get(user) !== null);
 
-  const runtimeConfig = useRuntimeConfig();
+	const getURL = () => {
+		const redirectLocal = localStorage.getItem("redirectTo");
+		const origin = window.location.origin;
+		let url = redirectLocal
+			? `${origin}${{ redirectLocal }}`
+			: `http://localhost:3000${redirectLocal ?? "/"}`;
 
-  const getURL = () => {
-    const redirectLocal = localStorage.getItem('redirectTo');
-    const origin = window.location.origin;
-    let url =
-      redirectLocal
-      ? `${origin}${{redirectLocal}}`
-      : `http://localhost:3000${redirectLocal ?? '/'}`;
+		url = url.startsWith("http") ? url : `https://${url}`;
+		return url;
+	};
 
-    // Make sure to include `https://` when not localhost.
-    url = url.startsWith('http') ? url : `https://${url}`
-    // Make sure to include a trailing `/`.
-    // url = url.endsWith('/') ? url : `${url}/`
-    return url
-  }
+	const loginWithProvider = async () => {
+		try {
+			const { data, error } = await client.auth.signInWithOAuth({
+				provider: "google",
+				options: {
+					redirectTo: getURL(),
+					scopes: "https://www.googleapis.com/auth/calendar.events",
+					queryParams: {
+						access_type: "offline",
+						prompt: "consent",
+					},
+				},
+			});
 
-  const loginWithProvider =  async () => {
-    try {
-      const { data, error } = await client.auth.signInWithOAuth({
-        provider: 'google',
-        options: {
-          redirectTo: getURL(),
-          scopes: 'https://www.googleapis.com/auth/calendar.events',
-          queryParams: {
-            access_type: 'offline',
-            prompt: 'consent',
-          },
-        }
-      })
-  
-      if(error){
-        throw createError({
-          message: error.message
-        })
-      }
-    } catch (err: any) {
-      throw createError({
-        message: err.message
-      })
-    }
-  }
+			if (error) {
+				throw error;
+			}
+		} catch (err: any) {
+			throw createError({
+				message: "Erro no login com o Google.",
+				cause: err,
+			});
+		}
+	};
 
-  const logout = async () => {
-    try {
-      localStorage.clear();
-      let { error } = await client.auth.signOut();
-  
-      if (error) {
-        throw error;
-      }
-  
-    } catch (e: any) {
-      createError({
-        statusText: e.code
-      })
-    }
-  }
+	const logout = async () => {
+		try {
+			localStorage.clear();
+			let { error } = await client.auth.signOut();
 
-  return {
-    isLogged,
-    loginWithProvider,
-    logout
-  }
-}
+			if (error) {
+				throw error;
+			}
+		} catch (err: any) {
+			createError({
+				message: "Erro no logout da aplicação.",
+				statusText: err.code,
+				cause: err,
+			});
+		}
+	};
+
+	return {
+		isLogged,
+		loginWithProvider,
+		logout,
+	};
+};
