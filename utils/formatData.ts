841e149bb9891a8766dddfe91f8f74b2e9426cd7
Italo Canturil <italo.cantur@gmail.com>
2025-04-03 21:32:04 -0300

    feat: create `camelToSnake` function

--- utils/formatData.ts
+++ utils/formatData.ts
@@ -1,24 +1,37 @@
 export const formatHour = (hour: string) => {
-  const date = new Date(hour).toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo', hour12: false});
+	const date = new Date(hour).toLocaleString("pt-BR", {
+		timeZone: "America/Sao_Paulo",
+		hour12: false,
+	});
 
-  const [hr, mn] = date.split(',')[1].split(':');
+	const [hr, mn] = date.split(",")[1].split(":");
 
-  return `${hr}:${mn}`
-}
+	return `${hr}:${mn}`;
+};
 
 export const formatDate = (hour: string) => {
-  const date = new Date(hour).toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo', hour12: false});
+	const date = new Date(hour).toLocaleString("pt-BR", {
+		timeZone: "America/Sao_Paulo",
+		hour12: false,
+	});
 
-  return date.split(',')[0]
-}
+	return date.split(",")[0];
+};
 
 export const formatDateWithHour = (hour: string) => {
-  const dateWithTime = `${hour}T00:00:00`;
-  const date = new Date(dateWithTime).toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo', hour12: false});
+	const dateWithTime = `${hour}T00:00:00`;
+	const date = new Date(dateWithTime).toLocaleString("pt-BR", {
+		timeZone: "America/Sao_Paulo",
+		hour12: false,
+	});
 
-  return date.split(',')[0]
-}
+	return date.split(",")[0];
+};
 
 export const stripTimeAndTimezone = (dateTime: string): string => {
-  return new Date(dateTime).toISOString().split('T')[0];
+	return new Date(dateTime).toISOString().split("T")[0];
+};
+
+export const camelToSnake = (str: string) => {
+	return str.replace(/[A-Z]/g, (letter) => `_${letter.toLocaleLowerCase()}`);
 };
