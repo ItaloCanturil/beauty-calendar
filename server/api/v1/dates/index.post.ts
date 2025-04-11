45f83a514a51c4f3f93852b1168eab512335f272
Italo Canturil <italo.cantur@gmail.com>
2025-04-11 11:42:01 -0300

    refactor: change post and get `/dates` route api

--- server/api/v1/dates/index.post.ts
+++ server/api/v1/dates/index.post.ts
@@ -1,22 +1,34 @@
-import { serverSupabaseClient, serverSupabaseUser } from '#supabase/server'
-import { IDate } from '~/stores/models/admin';
+import { serverSupabaseClient, serverSupabaseUser } from "#supabase/server";
+import { IDate } from "~/stores/models/admin";
 
 export default defineEventHandler(async (event) => {
-  const supabase = await serverSupabaseClient(event);
-  const user = await serverSupabaseUser(event);
+	const supabase = await serverSupabaseClient(event);
+	const user = await serverSupabaseUser(event);
 
-  const body = await readBody(event);
-  const datesArray = body.dates;
+	const body = await readBody(event);
+	const datesArray = body.dates;
 
-  if (!user) throw createError({ statusCode: 400, statusMessage: 'Usuário não logado'});
+	if (!user)
+		throw createError({ statusCode: 400, statusMessage: "Usuário não logado" });
 
-  if (!datesArray || datesArray.length === 0) throw createError({ statusCode: 400, statusMessage: 'Array de datas é obrigatório', data: datesArray});
+	if (!datesArray || datesArray.length === 0)
+		throw createError({
+			statusCode: 400,
+			statusMessage: "Array de datas é obrigatório",
+			data: datesArray,
+		});
 
-  const { data, error } = await supabase
-    .from('date_available')
-    .insert(datesArray.map((date: IDate) => (date)));
+	const { data, error } = await supabase
+		.from("date_available")
+		.insert(datesArray.map((date: IDate) => date));
 
-  if (error) throw createError({ statusText: error.code , statusMessage: error.message })
+	if (error)
+		throw createError({
+			statusText: error.code,
+			data: error,
+			statusCode: 400,
+			message: error.message ? error.message : "Erro ao salvar os horários.",
+		});
 
-  return data;
-})
+	return data;
+});
